294b38467d229acbd3f229767b4ecabc6f1c23f8
anashandaru <anashandaru256@gmail.com>
2017-04-27 08:16:12 +0700

    add comment to each process

--- seis2tilt.py
+++ seis2tilt.py
@@ -9,15 +9,21 @@ from obspy.core import read, Trace, Stream, UTCDateTime
 from obspy.signal.invsim import paz_to_freq_resp
 import numpy as np
 import csv
-import matplotlib.pyplot as plt
 
 startCut = "20161205T000000.0"
 endCut = "20161231T235959.9"
 
+# Set seismic and tilt data file path respectively
 ijonPath = r'/home/anas/Documents/BPPTKG/Seismometer2Tilt/IJO/satuBulan/ijob-n/*.msd'
 ijoTiltPath = r'/home/anas/Documents/BPPTKG/Seismometer2Tilt/IJO/gunungijo2.csv'
 
+# Read seismic data and return to ijon
 ijon = read(ijonPath)
+ijon.resample(7); # resample seismic raw data
+ijon.merge() # merge seismic data
+
+
+# Read tilt data and return to ijot
 ijotRaw = open(ijoTiltPath,'r')
 ijotRead = csv.reader(ijotRaw, delimiter=',')
 ijotList = []
@@ -35,12 +41,6 @@ ijotArray = np.array(ijotList)
 ijotTrace = Trace(data=ijotArray,header=stats)
 #jotTrace.stats.delta = 60
 ijot = Stream(ijotTrace)
-ijot = ijot.slice(starttime = UTCDateTime(startCut), endtime = UTCDateTime(endCut))
-ijot.plot()
-#print ijotRead[0][0]
-#ijotStats.starttime = ijotList[0].strip().split()[0]
-ijon.resample(7);
-ijon.merge()
 
 #poles = [-0.149+0.149j,-0.149-0.149j,-503+0j,-1010+0j,-1130+0j]
 poles = [-0.01178+0.01178j, -0.01178-0.01178j, -160+0j, -80+0j, -180+0j]
@@ -61,7 +61,7 @@ paz_cmg40T = {'poles': poles,
               'gain': 2304000.0,
               'sensitivity': 2*2977.0*1e6}
 
-# Apply Instrument Correction to each channel
+# Apply Instrument Correction
 ijon.simulate(paz_remove=paz_cmg40T,remove_sensitivity = True, taper = True, zero_mean=True,simulate_sensitivity=True)
 
 ijon.filter('highpass', freq = 0.000055, corners=2, zerophase=True)
@@ -73,13 +73,21 @@ ijonA = ijon.copy()
 # Calculate Acceleration value
 ijonA.differentiate(method = 'gradient')
 
+# Calculate Tilt Value
 tilt = ijonA[0].data/9.81
 trace = Trace(data=tilt,header=stats)
 ijonTs = Stream(trace)
 
+# cut all the data to specific time range
 ijon = ijon.slice(starttime = UTCDateTime(startCut), endtime = UTCDateTime(endCut))
 ijonA = ijonA.slice(starttime = UTCDateTime(startCut), endtime = UTCDateTime(endCut))
 ijonTs = ijonTs.slice(starttime = UTCDateTime(startCut), endtime = UTCDateTime(endCut))
+ijot = ijot.slice(starttime = UTCDateTime(startCut), endtime = UTCDateTime(endCut))
+
+# Plot Seismic Data
 #ijon.plot()
 #ijonA.plot()
 ijonTs.plot()
+
+# Plot tilt data
+ijot.plot()
